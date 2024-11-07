3c5b5c9e31ca5716ad64e6db36122261ba7d02e5
KevinDeNotariis <kevin.denotariis@gmail.com>
2024-11-07 14:38:52 +0100

    fix: allow to specify an integration for the root resource /

--- modules/rest-apigateway-resources/outputs.tf
+++ modules/rest-apigateway-resources/outputs.tf
@@ -1,6 +1,7 @@
 output "rest_apigateway_resources_map" {
   description = "A map associating each api path to its resource id"
   value = merge(
+    { "/" = var.rest_apigateway_root_resource_id },
     { for resource in aws_api_gateway_resource.layer_1 : trimprefix(resource.path, "/") => resource.id },
     { for resource in aws_api_gateway_resource.layer_2 : trimprefix(resource.path, "/") => resource.id },
     { for resource in aws_api_gateway_resource.layer_3 : trimprefix(resource.path, "/") => resource.id },
