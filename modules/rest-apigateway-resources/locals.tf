d05a309758480d8c2304d39ee3e1172f6f928560
KevinDeNotariis <kevin.denotariis@gmail.com>
2024-11-07 14:53:07 +0100

    fix: fixing check for root /

--- modules/rest-apigateway-resources/locals.tf
+++ modules/rest-apigateway-resources/locals.tf
@@ -1,7 +1,7 @@
 locals {
   integration_resources_map = merge({
     layer_1 = toset([
-      for key, config in var.rest_apigateway_lambdas : split("/", config.integration_full_path)[0] if config.integration_full_path[0] != ""
+      for key, config in var.rest_apigateway_lambdas : split("/", config.integration_full_path)[0] if config.integration_full_path != "/"
     ])
     }, {
     for num in [2, 3, 4, 5, 6, 7, 8, 9, 10] : "layer_${num}" => toset([
